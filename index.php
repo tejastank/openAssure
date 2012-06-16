cf7cf859e6840b6b050e8489e70d1d460473dcbc
teju <teju@teju-laptop.(none)>
2012-06-16 19:17:22 +0530

    smarty successful configured

--- index.php
+++ index.php
@@ -1,8 +1,19 @@
 <?php
+error_reporting(E_ALL);
 include_once("libs/Smarty-3.1.10/libs/Smarty.class.php");
 
 $smarty = new Smarty;
 
+$smarty->setTemplateDir('./templates/');
+$smarty->setCompileDir('./templates_c/');
+$smarty->setConfigDir('./configs/');
+$smarty->setCacheDir('./cache/');
+//   $smarty->testInstall(); # testing installation of smarty.
+/*  please note that tempalates_c dicrecotry must have persmission to create / write too..
+ * 
+ */
+
+//
 // assign some content. This would typically come from
 // a database or other source, but we'll use static
 // values for the purpose of this example.
@@ -10,13 +21,6 @@ $smarty->assign('name', 'george smith');
 $smarty->assign('address', '45th & Harris');
 
 // display it
-try{
-
-$smarty->display('header.tpl');
-
-}catch($e)
-{
-    print_r($e);
-}
+$smarty->display('templates/header.tpl');
 
 ?>
---
+++ templates_c/be439f82a4dbec61746f62a0df07c19a7eecd966.file.header.tpl.php
@@ -0,0 +1,35 @@
+<?php /* Smarty version Smarty-3.1.10, created on 2012-06-16 19:11:37
+         compiled from "templates/header.tpl" */ ?>
+<?php /*%%SmartyHeaderCode:8710848074fdc8d111cc789-69823869%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
+$_valid = $_smarty_tpl->decodeProperties(array (
+  'file_dependency' => 
+  array (
+    'be439f82a4dbec61746f62a0df07c19a7eecd966' => 
+    array (
+      0 => 'templates/header.tpl',
+      1 => 1339850970,
+      2 => 'file',
+    ),
+  ),
+  'nocache_hash' => '8710848074fdc8d111cc789-69823869',
+  'function' => 
+  array (
+  ),
+  'variables' => 
+  array (
+    'name' => 0,
+    'address' => 0,
+  ),
+  'has_nocache_code' => false,
+  'version' => 'Smarty-3.1.10',
+  'unifunc' => 'content_4fdc8d1146efb6_23102926',
+),false); /*/%%SmartyHeaderCode%%*/?>
+<?php if ($_valid && !is_callable('content_4fdc8d1146efb6_23102926')) {function content_4fdc8d1146efb6_23102926($_smarty_tpl) {?>
+User Information:
+
+Name: <?php echo $_smarty_tpl->tpl_vars['name']->value;?>
+
+Address: <?php echo $_smarty_tpl->tpl_vars['address']->value;?>
+
+
+<?php }} ?>
